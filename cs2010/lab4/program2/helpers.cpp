702f5323a3b9fe42bf1ead6e84befd5da561c6e8
reecehutchison <hutchisora@icloud.com>
2024-10-13 00:10:06 -0600

    very close to done

--- cs2010/lab4/program2/helpers.cpp
+++ cs2010/lab4/program2/helpers.cpp
@@ -17,10 +17,20 @@ void display_instructions();
 void debug_on_or_off();
 void debugger(int ch, MEVENT event);
 void initialize_ncurses();
-void starting_loop();
+void main_loop();
 void display_items();
 void display_grid(vector<vector<int>> grid);
 void add_alive(vector<vector<int>>& grid, int y, int x);
+void manual_mode(vector<vector<int>>& grid);
+void manual_instructions();
+void display_generation(int count);
+void print_man_items(int count);
+void next_generation(vector<vector<int>>& grid);
+int count_neighbors_bfs(vector<vector<int>> grid, int i, int j);
+void animation_mode(vector<vector<int>>& grid);
+void print_ani_items(int count, int speed);
+void animation_instructions();
+void display_speed(int speed);
 
 const int HEIGHT=32;
 const int WIDTH=64;
@@ -33,7 +43,6 @@ bool DEBUG_MODE=false;
 
 void game_of_life() {
 
-
     initialize_ncurses();
 
     clear();
@@ -41,8 +50,7 @@ void game_of_life() {
     if(DEBUG_MODE)
         mvprintw(46, 0, "waiting for input...");
 
-    starting_loop();
-
+    main_loop();
     
     endwin(); // ends ncurses system
 }
@@ -105,8 +113,8 @@ void initialize_ncurses() {
     curs_set(0); // disable cursor
 }
 
-void starting_loop() {
-    while (true) {
+void main_loop() {
+    while(true) {
         
         int ch=getch();
         clear();
@@ -119,9 +127,11 @@ void starting_loop() {
                 add_alive(GRID, y, x);
             }    
         } else if((ch-'0')==1) {
-            // animated
-        } else if((ch-'0')==2) {
-            // manual mode
+            animation_mode(GRID);
+            break;                // note: these two break out
+        } else if((ch-'0')==2) { //        after modes since 
+            manual_mode(GRID);      //         program ends.
+            break;
         } else if((ch-'0')==3) {
             if(DEBUG_MODE==false)
                 DEBUG_MODE=true;
@@ -173,6 +183,139 @@ void add_alive(vector<vector<int>>& grid, int y, int x) {
             mvprintw(0, 0, "grid NOT updated");
     }
 }
+
+void manual_mode(vector<vector<int>>& grid) {
+
+    int count=1;
+    print_man_items(0);
+
+    while(true) {
+    
+        int ch=getch();
+
+        if(ch=='q') 
+            break;
+
+        next_generation(GRID);
+        print_man_items(count);
+
+        ++count; 
+        refresh();
+    }
+}
+
+void manual_instructions() {
+    mvprintw(4, 20, "Press any key to move to the next generation.");
+}
+
+void display_generation(int count) {
+    mvprintw(8, 16, "Generation : %d", count);
+}
+
+void print_man_items(int count) {
+        clear(); 
+        display_title();
+        display_border();
+        display_grid(GRID);
+        manual_instructions();
+        mvprintw(5, 26, "Press 'q' to quit the program.");
+        display_generation(count);
+}
+
+void next_generation(vector<vector<int>>& grid) {
+    int n=grid.size();
+    int m=grid[0].size();
+    vector<vector<int>> new_grid=grid;
+    for(int i=0; i<n; ++i) {
+        for(int j=0; j<m; ++j) {
+            int neighbors=count_neighbors_bfs(GRID, i, j);
+            if(grid[i][j]==1) {
+                if(neighbors<2||neighbors>3)
+                    new_grid[i][j]=0;
+            } else {
+                if(neighbors==3)
+                    new_grid[i][j]=1;
+            }
+        }
+    }
+    grid=new_grid;
+}
+
+int count_neighbors_bfs(vector<vector<int>> grid, int i, int j) {
+
+   int count=0;                      // note: this is just a bfs 
+   int rows=grid.size();            //        to a depth of 1. 
+   int cols=grid[0].size();
+
+   vector<pair<int, int>> directions={
+       {-1, -1}, {-1, 0}, {-1, 1},
+        {0, -1}, /*curr*/  {0, 1},
+        {1, -1},  {1, 0},  {1, 1}
+    };
+
+   // bfs to depth of 1
+   for(const auto& dir : directions) {
+       int ni=i+dir.first;                // neighbor indexs initialized
+       int nj=j+dir.second;
+
+   if(ni>=0&&ni<rows&&nj>=0&&nj<cols)    // in bounds check
+       count+=grid[ni][nj];
+    }
+   return count;
+}
+
+void animation_mode(vector<vector<int>>& grid) {
+    int count=1;    
+    int delay_speed=100;
+    nodelay(stdscr, TRUE); // turn off waiting for getch
+    print_ani_items(0, delay_speed);
+
+    while(true) {
+
+        int ch=getch();
+
+        if(ch=='q')
+            break;
+        else if((ch-'0')==1) {
+            if(delay_speed<200)
+                delay_speed+=25;
+        } else if((ch-'0')==2) {
+            if(delay_speed>25) 
+                delay_speed-=25;
+        }
+
+        napms(delay_speed);
+        next_generation(GRID);
+        print_ani_items(count, delay_speed);
+        ++count;
+        refresh();
+    }
+}
+
+void print_ani_items(int count, int speed) { 
+        clear(); 
+        display_title();
+        display_border();
+        display_grid(GRID);
+        animation_instructions();
+        mvprintw(5, 26, "Press 'q' to quit the program.");
+        display_generation(count);
+        display_speed(speed);
+}
+
+void animation_instructions() {
+    mvprintw(4, 20, "Press keys to change Animation Mode options. ");
+    mvprintw(7, 45, "1. Increase speed");
+    mvprintw(8, 45, "2. Decrease speed");
+}
+
+void display_speed(int speed) {
+    mvprintw(7, 16, "Speed : %d", speed);
+}
+
+
+
+
 
 
 
